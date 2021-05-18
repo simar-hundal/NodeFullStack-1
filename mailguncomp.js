fefde6c46688f357b6df0012527e24924edc3959
Dheeraj Singh <bond.deejay@gmail.com>
2021-05-18 13:54:30 +0530

    Updated the working copy of mailgun
    dear all this is Updated the working copy of sending the mail from react js mailgun without a node backend support. this implementation is much faster than your backend implementation using node js

--- mailguncomp.js
+++ mailguncomp.js
@@ -1,31 +1,31 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
-export default function MailGunCompoenet() {
+export default function MyApp() {
   const [from, setFrom] = useState('')
   const [to, setTo] = useState('')
   const [subject, setSubject] = useState('')
   const [body, setBody] = useState('')
+  const [result, setResult] = useState('')
 
-  const mailgun = require('mailgun-js')
-  //const DOMAIN = "309a201a929ec35b5d073ab1294ca1a8-602cc1bf-cd18fc46";
-  const mg = mailgun({
-    apiKey: '309a201a929ec35b5d073ab1294ca1a8-602cc1bf-cd18fc46',
-    domain: 'sandboxdd54c1e250304213831e0df577224269.mailgun.org',
-  })
+  //const DOMAIN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+  const mg = {
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    domain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org',
+  }
+
   const data = {
-    from: 'Your Custom name User <me@samples.mailgun.org>',
-    to: 'ladijayshree7@gmail.com',
+    from: 'test@' + mg.domain,
+    to: 'bond.deejay@gmail.com',
     subject: 'Hello',
     text: 'Testing some Mailgun awesomness!',
   }
-
   const handleInputChange = (e) => {
     let nameSwitch = e.target.name
-    e.preventDefault()
     switch (nameSwitch) {
       case 'from': {
         setFrom(e.target.value)
-        data.from = e.target.value
+        //  data.from = e.target.value;
         break
       }
 
@@ -49,17 +49,33 @@ export default function MailGunCompoenet() {
       }
     }
   }
-  const onFormsubmit = (e) => {
-    console.log(body)
-    try {
-      mg.messages().send(body, function (error, body) {
-        console.log(body)
-        console.log(error)
-      })
-    } catch (error) {
-      console.log()
+  function requestObject(url, method, params) {
+    let configObject = {
+      url: url,
+      method: method,
+      params: params,
+      auth: {
+        username: 'api',
+        password: 'xxxxxxxxxxxxxxxxxxxxxx-xxxxxxxx-xxxxxxxx',
+      },
     }
+    return configObject
   }
+  async function onFormsubmit(e) {
+    console.log(data)
+    console.log(requestObject())
+    let configObejct = requestObject(
+      'https://api.mailgun.net/v3/sandboxxxxxxxxxxxxx-xxxxxxxx-xxxxxxxx.mailgun.org/messages',
+      'post',
+      data
+    )
+    axios.request(configObejct).then((res) => {
+      console.log('react1: ', res)
+      console.log('react2: ', res.data)
+      setResult(res.data.message)
+    })
+  }
+
   return (
     <div className='App'>
       <h1>Hello CodeSandbox</h1>
@@ -68,7 +84,7 @@ export default function MailGunCompoenet() {
       <p>Subject - {subject}</p>
       <p>To - {to}</p>
       <p>Body - {body}</p>
-      <form onSubmit={onFormsubmit} action='#'>
+      <form>
         <input
           onChange={handleInputChange}
           placeholder='FRom Email Address'
@@ -89,8 +105,9 @@ export default function MailGunCompoenet() {
         <br />
         <input onChange={handleInputChange} placeholder='body' name='body' />
         <br />
-        <button>Save Data</button>
       </form>
+      <button onClick={onFormsubmit}>Save Data</button>
+      {result}
     </div>
   )
 }
